bc3cd1db0f7f6e980c6f7e661d0f1e97f5fde2c9
cZhen42 <chaobinzhen@gmail.com>
2017-11-14 04:49:55 -0500

    Create empty point object

--- Assets/Script/Config.cs
+++ Assets/Script/Config.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Config : MonoBehaviour {
     public static float rampAngle = 15.0f;
+    public static int row = 30;
+    public static int col = 16;
+    public static float radBobble = 0.15f;
+    public static float offsetHeight = 0.07068966f;
 	// Use this for initialization
 	void Start () {
 		
--- Assets/Script/CreateBobble.cs
+++ Assets/Script/CreateBobble.cs
@@ -10,22 +10,77 @@ public class CreateBobble : MonoBehaviour
     public GameObject[] bobbleStyle = new GameObject[5];
     public int layerMaxBallNum = 5;
 
-    private GameObject bobbleObject;
+    private GameObject randBobble;
+    private GameObject leftCorner;
+    private Vector3 leftCornerPos;
+
+    public struct bobble
+    {
+        public GameObject pointObject;
+        public GameObject bobbleObject;
+    }
+
+    public bobble[,] m_bobble;
+
+    private int row = Config.row;
+    private int col = Config.col;
+
 
 	// Use this for initialization
 	void Start () {
         Instance = this;
+        m_bobble = new bobble[row, col];
+        leftCorner = GameObject.Find("Left Corner");
+        leftCornerPos = leftCorner.transform.position;
+
+        // Initiate bobbles
+        Init(3);
         //  Create bobbles ready to shoot
         Vector3 shootPos = GameObject.FindGameObjectWithTag("Muzzle").transform.position;
-        bobbleObject = bobbleStyle[Random.Range(0, layerMaxBallNum)];
-        shootBobble[0] = Instantiate(bobbleObject, shootPos, Quaternion.identity) as GameObject;
+        randBobble = bobbleStyle[Random.Range(0, layerMaxBallNum)];
+        shootBobble[0] = Instantiate(randBobble, shootPos, Quaternion.identity) as GameObject;
         shootPos = GameObject.Find("Loading").transform.position;
-        bobbleObject = bobbleStyle[Random.Range(0, layerMaxBallNum)];
-        shootBobble[1] = Instantiate(bobbleObject, shootPos, Quaternion.identity) as GameObject;
+        randBobble = bobbleStyle[Random.Range(0, layerMaxBallNum)];
+        shootBobble[1] = Instantiate(randBobble, shootPos, Quaternion.identity) as GameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    // Initiate base
+    public void Init(int num) {
+        // Clear the arrays
+        for (int j = 0; j < col; j++) {
+            for (int i = 0; i < row; i++) {
+                if (m_bobble[i, j].bobbleObject != null)
+                {
+                    Destroy(m_bobble[i, j].bobbleObject);
+                    m_bobble[i, j].bobbleObject = null;
+                }
+                if (m_bobble[i, j].pointObject != null)
+                {
+                    Destroy(m_bobble[i, j].pointObject);
+                    m_bobble[i, j].pointObject = null;
+                }
+            }
+        }
+        // Create point for bobble stop postion
+        for (int i = 0; i < row; i++) {
+            if(i % 2 == 0) {
+                for (int j = 0; j < col; j++) {
+                    m_bobble[i, j].pointObject = Instantiate(leftCorner, new Vector3(leftCornerPos.x + i * 1.732050f * Config.radBobble, leftCornerPos.y - i * Config.offsetHeight, leftCornerPos.z + j * 2 * Config.radBobble), Quaternion.identity);
+                    m_bobble[i, j].bobbleObject = null;
+                }
+            }
+            if(i % 2 == 1) {
+                for (int j = 0; j < (col - 1); j++) {
+                    m_bobble[i, j].pointObject = Instantiate(leftCorner, new Vector3(leftCornerPos.x + i * 1.732050f * Config.radBobble, leftCornerPos.y - i * Config.offsetHeight, leftCornerPos.z + j * 2 * Config.radBobble + Config.radBobble), Quaternion.identity);
+                    m_bobble[i, j].bobbleObject = null;
+                }
+            }
+
+        }
+    }
 }
